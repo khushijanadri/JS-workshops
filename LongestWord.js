18c0b1dcc8f6d4bef515ccdc9d06c3113b726adb
Khushi <jk18022008@gmail.com>
2026-07-16 11:03:45 +0530

    Add function to find the length of the longest word

---
+++ LongestWord.js
@@ -0,0 +1,13 @@
+function findLongestWordLength(string)
+{
+  const words = string.split(' ');
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
